9c5dd483716d72f5ce796876dad777747ca39e42
otoker44047 <r13944047@ntu.edu.tw>
2025-03-05 16:02:50 +0800

    add harpoon and comment utility

---
+++ lua/custom/plugins/harpoon.lua
@@ -0,0 +1,12 @@
+-- dd to delete the marked file inside the TAB UI
+return {
+    "ThePrimeagen/harpoon", 
+    event = "VeryLazy",
+    dependencies = {
+        "nvim-lua/plenary.nvim"
+    },
+    config = function()
+        vim.keymap.set("n", "<s-m>", "<cmd>lua require('harpoon.mark').add_file()<cr>", {desc = "Harpoon Mark File"})
+        vim.keymap.set("n", "<TAB>", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", {desc = "Harpoon Toggle Menu"})
+    end
+}
